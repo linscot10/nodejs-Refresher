b5e86248fb22304f1363db9aa042a0f4932b4ef4
linscot10 <linscot10@gmail.com>
2025-01-11 19:56:18 +0300

    User registration done

--- nodeJS_auth/controllers/auth-controllers.js
+++ nodeJS_auth/controllers/auth-controllers.js
@@ -1,7 +1,50 @@
-const express = require("express")
+const User = require("../database/db")
+const bcrypt = require("bcryptjs")
 
 const registerUser = async () => {
     try {
+
+        const { userName, email, password, role } = req.body;
+        const checkExistingUser = await User.findOne({ $or: [{ userName }, { email }] })
+
+        if (checkExistingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "User with this Username or Id already exists"
+            })
+        }
+        // password hashing
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // user creation
+
+        const newlyCreatedUser = new User({
+            userName,
+            email,
+            password: hashedPassword,
+            role: role || 'user'
+        })
+
+        await newlyCreatedUser.save();
+
+        if (newlyCreatedUser) {
+            res.status(201).json({
+                success: true,
+                message: "User created successfully"
+            })
+        }
+        else {
+            res.status(201).json({
+                success: false,
+                message: " Unable to register User! Please try again"
+            })
+        }
+
+
+
+
+
 
     } catch (error) {
         res.status(500).json({
