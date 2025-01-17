bcfa30794747636e03b547eade3ae630a5f4415b
linscot10 <linscot10@gmail.com>
2025-01-17 12:36:40 +0300

    authorization

--- nodeJS_auth/middleware/auth.middleware.js
+++ nodeJS_auth/middleware/auth.middleware.js
@@ -1,8 +1,31 @@
+const jwt = require("jsonwebtoken")
+
+
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers["authorization"]
     console.log(authHeader);
-co
-    next()
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: "Access Denied ,No token Provided .Please Login to Coninue"
+        })
+    }
+
+    try {
+        const decodeTokenInfo = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        console.log(decodeTokenInfo);
+        req.userInfo = decodeTokenInfo;
+        next()
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Access Denied ,No token Provided .Please Login to Coninue"
+        })
+    }
+   
 }
 
 module.exports = authMiddleware;
