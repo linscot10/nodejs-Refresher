aa0c81e0b85d1c59d77d563199570649aa3de557
linscot10 <linscot10@gmail.com>
2025-01-19 23:17:41 +0300

    pagination and sorting

--- nodeJS_auth/controllers/image-controllers.js
+++ nodeJS_auth/controllers/image-controllers.js
@@ -1,7 +1,7 @@
 const Image = require('../model/image')
 const { uploadToCloudinary } = require("../helpers/cloudinaryHelpers")
 const fs = require("fs")
-const cloudinary = 
+const cloudinary = require("../config/cloudinary")
 
 const uploadImageController = async (req, res) => {
     try {
@@ -42,10 +42,27 @@ const uploadImageController = async (req, res) => {
 
 const fetchImagesContoller = async (req, res) => {
     try {
-        const images = await Image.find({})
+        const page = parseInt(req.query.page) || 1
+        const limit = parseInt(req.query.limit) || 5;
+        const skip = (page - 1) * limit;
+        const sortBy = req.query.sortBy || 'createdAt';
+        const sortOrder = req.query.sortOrder === 'async' ? 1 : -1;
+        const totalImages = await Image.countDocuments();
+        const totalPages = Math.ceil(totalImages / limit);
+
+        const sortObj = {}
+        sortObj[sortBy] = sortOrder
+
+        const images = await Image.find().sort(sortObj).skip(skip).limit(limit)
+        // const images = await Image.find({})
         res.status(200).json({
             success: true,
-            image: images
+            data: images,
+            totalPages: totalPages,
+            totalImages: totalImages,
+            currentPage: page,
+
+
         })
     } catch (error) {
         console.log(error)
@@ -69,12 +86,24 @@ const deleteImageController = async (req, res) => {
                 message: "Image Not Found"
             })
         }
+
+
         if (image.uploadedBy.toString() !== userId) {
             return res.status(403).json({
                 success: false,
                 message: "You are not authorized to delete this image "
             })
         }
+
+
+        await cloudinary.uploader.destroy(image.publicId)
+
+        await Image.findByIdAndDelete(getCurrentIdOfImageToBeDeleted)
+
+        res.status(200).json({
+            success: true,
+            message: "Image deleted successfully"
+        })
     } catch (error) {
         console.log(error)
         res.status(500).json({
@@ -85,5 +114,6 @@ const deleteImageController = async (req, res) => {
 }
 module.exports = {
     uploadImageController,
-    fetchImagesContoller
+    fetchImagesContoller,
+    deleteImageController
 }
--- nodeJS_auth/routes/image-routes.js
+++ nodeJS_auth/routes/image-routes.js
@@ -2,10 +2,11 @@ const express = require("express")
 const authMiddleware = require("../middleware/auth.middleware");
 const adminMiddleware = require('../middleware/admin-middleware')
 const uploadMiddleware = require("../middleware/upload-middlware")
-const { uploadImageController, fetchImagesContoller } = require('../controllers/image-controllers')
+const { uploadImageController, fetchImagesContoller, deleteImageController } = require('../controllers/image-controllers')
 const router = express.Router()
 
 router.post('/upload', authMiddleware, adminMiddleware, uploadMiddleware.single('image'), uploadImageController)
 router.get('/get', authMiddleware, fetchImagesContoller)
+router.delete('/:id', authMiddleware, adminMiddleware, deleteImageController)
 
 module.exports = router
