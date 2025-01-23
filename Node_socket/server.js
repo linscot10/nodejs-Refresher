419d78d532a49db3ba0c874d8c4abd560ac62f0d
linscot10 <linscot10@gmail.com>
2025-01-23 09:48:33 +0300

    nodejs with socket.io

--- Node_socket/server.js
+++ Node_socket/server.js
@@ -18,4 +18,17 @@ const users = new Set()
 io.on("connection", (socket) => {
     console.log("a user is now connected");
 
+    socket.on('join', (userName) => {
+        users.add(userName)
+
+        io.emit('userJoined', userName)
+
+        io.emit('userList', Array.from(users))
+    })
+
+})
+
+server.listen(3000, () => {
+    console.log(`server is now running on PORT: http://localhost:${3000}`);
+
 })
