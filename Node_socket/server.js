7cc7e68c1cdbeb2c53444292b319ffc3ae95da79
linscot10 <linscot10@gmail.com>
2025-01-23 09:11:31 +0300

    nodejs with socket.io

---
+++ Node_socket/server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const http = require("http")
+const socketIo = require("socket.io")
+
+
+const app = express()
+
+
+const server = http.createServer(app)
+
+const io = socketIo(server)
+
+app.use(express.static('public'))
+
+
+const users = new Set()
+
+io.on("connection", (socket) => {
+    console.log("a user is now connected");
+
+})
